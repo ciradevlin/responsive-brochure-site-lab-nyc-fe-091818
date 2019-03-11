3e9b4a9a0e75bde778b3c537399f5221dba19383
Cira Devlin <cira.devlin@leveluprpo.com>
2019-03-11 23:52:51 +0000

    Automatically backed up by Learn

---
+++ website/index.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Original Code 
+
+// //Cheat code
+
+// const code = [77, 79, 78, 75, 69, 89]
+
+// let tracker = 0
+
+// function onKeyDown(event) {
+//   const keycode = event.which
+  
+//   // 1. figure out where they were
+//   if (keycode === code[tracker]) {
+//     tracker = tracker + 1
+//   } else {
+//     // 2. reset to the beginning if they mess up
+//     tracker = 0
+//   }
+//   // 3. we show a success message if they win
+//   if (tracker === 6) {
+//     alert('you have unlocked the secret to life')
+//   }
+  
+//   console.log(tracker)
+//   // 1. figure out where in the sequence they are 
+//   // 2. figure out if the key they pressed is right
+  
+// }
+
+// function init() {
+//   // Write your JavaScript code inside the init() function
+  
+//   document.body.addEventListener('keydown', onKeyDown)  
+// }
+
+// init()
+
